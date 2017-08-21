5892c455965d68bdfc26ebc31daeb455349a9ff5
anthony-pretto <anthony@pretto.fr>
2017-08-21 13:17:16 +0200

    style for testimonies

--- js/slider.js
+++ js/slider.js
@@ -3,22 +3,26 @@ $(document).ready(function(){
     var contentWidth = $( window ).outerWidth();
     if(contentWidth < 840){
       maxSlides = 1;
+      slideMargin = 16;
     } else {
       maxSlides = 2;
+      slideMargin = 24;
     }
     return maxSlides;
   }
   var maxSlides = calcParams();
-  var sliderWidth = 376;
+  var sliderWidth = 420;
   var slider = $('.home-testimonie-container').bxSlider({
     maxSlides: maxSlides,
-    slideWidth: sliderWidth
+    slideWidth: sliderWidth,
+    slideMargin: slideMargin
   });
   $(window).resize(function(){
     var maxSlides = calcParams();
     slider.reloadSlider({
       maxSlides: maxSlides,
-      slideWidth: sliderWidth
+      slideWidth: sliderWidth,
+      slideMargin: slideMargin
     });
   });
 });
